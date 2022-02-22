0f525d641bfc2f86ac829ce6da279679c412d8ab
Connor Minton <connor.c.minton@gmail.com>
2022-02-21 21:13:28 -0500

    add test for FileUtils::readHackFile

--- FileUtils_test.cpp
+++ FileUtils_test.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include <vector>
 
 #include "FileUtils.h"
+#include "Test.h"
+
+void test_readHackFile(TestContext& cx) {
+  std::vector<uint16_t> maxRom = {
+    0x0000, 0xfc10, 0x0001, 0xf4d0,
+    0x000a, 0xe301, 0x0001, 0xfc10,
+    0x000c, 0xea87, 0x0000, 0xfc10,
+    0x0002, 0xe308, 0x000e, 0xea87
+  };
+
+  auto actualRom = FileUtils::readHackFile("hack/Max.hack");
+
+  cx.expectEqual(actualRom, maxRom);
+}
 
 int main() {
-  FileUtils::readHackFile("asdf");
+  TestContext cx;
+
+  cx.test("FileUtils::readHackFile", test_readHackFile);
+
+  cx.printResults();
 }
